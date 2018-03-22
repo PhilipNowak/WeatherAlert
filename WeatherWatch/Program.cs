8948d436a9fbb17ca7ac5e7ef9d318d15084505e
Phil Nowak <pjnowak9@gmail.com>
2018-03-22 18:36:29 -0500

    Add project files.

---
+++ WeatherWatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
